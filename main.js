ff1231679a4704d9741f68d4088cb244472b2fac
jose-g315 <120354063+jose-g315@users.noreply.github.com>
2024-06-28 00:41:55 -0500

    Added initial html and css to help test along with defining the displayBooks() logic.

--- main.js
+++ main.js
@@ -12,12 +12,50 @@ function addBookToLibrary(title, author, pageNumber, genre, readStatus) {
   const book = new Book(title, author, pageNumber, genre, readStatus);
   myLibrary.push(book);
 }
-addBookToLibrary("test", "test1", "test3", "test4", "test5");
-addBookToLibrary("test21", "test1321", "test3321", "test4321", "test5321");
+addBookToLibrary("Eva is a Goose", "Daniel Gonzalez", "300", "Adventure", "Read");
+addBookToLibrary("Eva is a Goose", "Daniel Gonzalez", "300", "Adventure", "Read");
+
 
 function displayBooks(){
+  const bookShelf = document.querySelector(".bookshelf");
     myLibrary.forEach(book => {
-        console.log(book);
+      // creating a card for every book in the array
+      const bookCard = document.createElement('div');
+      bookCard.classList.add("book-card");
+      
+      /* creating an element and inputting corresponding book 
+         value then adding class and appending to the card */
+      const title = document.createElement('p');
+      title.textContent = book.title;
+      title.classList.add("title");
+      bookCard.appendChild(title);
+
+      const author = document.createElement('p');
+      author.textContent = book.author;
+      author.classList.add("author");
+      bookCard.appendChild(author);
+
+      const pageNumber = document.createElement('p');
+      pageNumber.textContent = book.pageNumber;
+      pageNumber.classList.add("page-number");
+      bookCard.appendChild(pageNumber);
+
+      const genre = document.createElement("p");
+      genre.textContent = book.genre;
+      genre.classList.add("genre");
+      bookCard.appendChild(genre);
+
+      const readStatus = document.createElement('div');
+      readStatus.textContent = book.readStatus;
+      readStatus.classList.add("read-status");
+      bookCard.appendChild(readStatus);
+      
+      const deleteButton = document.createElement('div');
+      deleteButton.classList.add("delete-button");
+      bookCard.appendChild(deleteButton);
+
+      // adding each card to the shelf
+      bookShelf.append(bookCard);
     });
 }
 displayBooks();
